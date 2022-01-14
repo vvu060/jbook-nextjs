69fedc403a996ab588c5f925f44b2c9c4774b4db
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-14 13:05:42 +0530

    add code to use esbuild to and custom plugins to build and bundle the code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
-import { useState, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useRef, useEffect } from 'react';
+import { fetchPlugin } from '../plugins/fetch-plugin';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 
 const Home = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
   const onClick = async () => {
     if (!ref.current) return;
 
-    setCode('Dummy Text');
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    setCode(result);
   };
 
   return (
