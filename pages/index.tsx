01d1ed190711543f77dcc1869e67abbe167a1ae9
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-16 22:41:02 +0530

    refactor bundler code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,18 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useState, useRef, useEffect } from 'react';
 
-import { fetchPlugin } from '../plugins/fetch-plugin';
-import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { useState } from 'react';
 
+import bundle from 'bundler';
 import CodeEditor from '@/components/CodeEditor';
 import Preview from '@/components/Preview';
 
 const Home = () => {
-  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) return;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
