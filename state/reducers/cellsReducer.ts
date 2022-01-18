f2a5e570b72009d2afaad8ae280e859f0060a753
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-18 16:35:54 +0530

    add logic for other action types

--- state/reducers/cellsReducer.ts
+++ state/reducers/cellsReducer.ts
@@ -13,6 +13,10 @@ interface CellsState {
   };
 }
 
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5);
+};
+
 const initialState: CellsState = {
   loading: false,
   error: null,
@@ -29,13 +33,42 @@ const reducer = produce(
         return;
 
       case ActionType.DELETE_CELL:
-        return state;
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
+        return;
 
       case ActionType.MOVE_CELL:
-        return state;
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+        if (targetIndex < 0 || targetIndex >= state.order.length - 1) return;
+
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+
+        return;
 
       case ActionType.INSERT_CELL_BEFORE:
-        return state;
+        const cell: Cell = {
+          content: '',
+          type: action.payload.type,
+          id: randomId(),
+        };
+
+        state.data[cell.id] = cell;
+
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id,
+        );
+
+        if (foundIndex < 0) {
+          state.order.push(cell.id);
+        } else {
+          state.order.splice(foundIndex, 0, cell.id);
+        }
+
+        return;
 
       default:
         return state;
