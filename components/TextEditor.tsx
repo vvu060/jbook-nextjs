1df8f0f6c9356a52843e30dcfc355882d6522360
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-18 11:42:33 +0530

    toggle markdown editor

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,9 +1,32 @@
 import MDEditor from '@uiw/react-md-editor';
+import { useState, useEffect } from 'react';
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
     </div>
   );
 };
