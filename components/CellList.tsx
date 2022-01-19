005e6cb4c12b65c0dd74272e051ddf95bf992413
Vishal Urankar <vishalurankar@Vishals-MacBook-Air.local>
2022-01-19 11:22:25 +0530

    get ordered list

--- components/CellList.tsx
+++ components/CellList.tsx
@@ -1,7 +1,7 @@
 import { useTypedSelector } from 'hooks/use-typed-selector';
 
 const CellList: React.FC = () => {
-  useTypedSelector((state) => state);
+  useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
 
   return <div>CellList</div>;
 };
